ed30ed50cd65fba87fc54aa8be952b0ba348dd7c
minnas <minsalon@gmail.com>
2019-02-18 21:49:46 +0200

    updated plugins

--- src/index.js
+++ src/index.js
@@ -6,15 +6,20 @@ import blogStore from './store/blogStore'
 import App from './App'
 import BlogForm from './components/blogForm'
 import Navbar from './components/navbar'
+import Loader from './components/loader'
 //some basic styles
 import './css/bootstrap.min.css';
 import './css/index.css';
+import './css/test.css';
+
+
 
 const render = () => {
     ReactDOM.render(
         <Provider store={blogStore}>
             <BrowserRouter>
                 <div class="container">
+                     <Loader/>
                     <Navbar/>
                     <div class="row">
                         <div class="col-12">                    
